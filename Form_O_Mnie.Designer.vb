1534b4a552a02e57a32a619add11ae2031c69535
excelsolutions <31018503+excelsolutions@users.noreply.github.com>
2021-03-05 11:10:41 +0100

    Add files via upload

--- Form_O_Mnie.Designer.vb
+++ Form_O_Mnie.Designer.vb
@@ -24,17 +24,15 @@ Partial Class Form_O_Mnie
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form_O_Mnie))
         Me.Btn_Zamknij = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.LinkMail = New System.Windows.Forms.LinkLabel()
-        Me.LinkWWW = New System.Windows.Forms.LinkLabel()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.L_Wersja = New System.Windows.Forms.Label()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LinkWWW = New System.Windows.Forms.LinkLabel()
+        Me.LinkMail = New System.Windows.Forms.LinkLabel()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -42,22 +40,12 @@ Partial Class Form_O_Mnie
         '
         'Btn_Zamknij
         '
-        Me.Btn_Zamknij.Location = New System.Drawing.Point(257, 257)
+        Me.Btn_Zamknij.Location = New System.Drawing.Point(42, 266)
         Me.Btn_Zamknij.Name = "Btn_Zamknij"
         Me.Btn_Zamknij.Size = New System.Drawing.Size(165, 35)
         Me.Btn_Zamknij.TabIndex = 0
         Me.Btn_Zamknij.Text = "Close"
         Me.Btn_Zamknij.UseVisualStyleBackColor = True
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.PDF_Reader.My.Resources.Resources.P1080372
-        Me.PictureBox1.Location = New System.Drawing.Point(-13, -1)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(224, 278)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 1
-        Me.PictureBox1.TabStop = False
         '
         'Label1
         '
@@ -67,35 +55,6 @@ Partial Class Form_O_Mnie
         Me.Label1.Size = New System.Drawing.Size(28, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "mail:"
-        '
-        'LinkMail
-        '
-        Me.LinkMail.AutoSize = True
-        Me.LinkMail.Location = New System.Drawing.Point(47, 27)
-        Me.LinkMail.Name = "LinkMail"
-        Me.LinkMail.Size = New System.Drawing.Size(148, 13)
-        Me.LinkMail.TabIndex = 3
-        Me.LinkMail.TabStop = True
-        Me.LinkMail.Text = "lukasz.r.morawski@gmail.com"
-        '
-        'LinkWWW
-        '
-        Me.LinkWWW.AutoSize = True
-        Me.LinkWWW.Location = New System.Drawing.Point(47, 100)
-        Me.LinkWWW.Name = "LinkWWW"
-        Me.LinkWWW.Size = New System.Drawing.Size(111, 13)
-        Me.LinkWWW.TabIndex = 7
-        Me.LinkWWW.TabStop = True
-        Me.LinkWWW.Text = "www.excelsolutions.pl"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 100)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(34, 13)
-        Me.Label3.TabIndex = 6
-        Me.Label3.Text = "www:"
         '
         'Label5
         '
@@ -110,7 +69,7 @@ Partial Class Form_O_Mnie
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.Label5)
-        Me.GroupBox1.Location = New System.Drawing.Point(220, 3)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(201, 71)
         Me.GroupBox1.TabIndex = 12
@@ -123,7 +82,7 @@ Partial Class Form_O_Mnie
         Me.GroupBox2.Controls.Add(Me.LinkMail)
         Me.GroupBox2.Controls.Add(Me.LinkWWW)
         Me.GroupBox2.Controls.Add(Me.Label3)
-        Me.GroupBox2.Location = New System.Drawing.Point(220, 128)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 137)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(201, 123)
         Me.GroupBox2.TabIndex = 13
@@ -133,7 +92,7 @@ Partial Class Form_O_Mnie
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.L_Wersja)
-        Me.GroupBox3.Location = New System.Drawing.Point(221, 74)
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 83)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Size = New System.Drawing.Size(201, 48)
         Me.GroupBox3.TabIndex = 14
@@ -150,20 +109,47 @@ Partial Class Form_O_Mnie
         Me.L_Wersja.Size = New System.Drawing.Size(195, 29)
         Me.L_Wersja.TabIndex = 10
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "www:"
+        '
+        'LinkWWW
+        '
+        Me.LinkWWW.AutoSize = True
+        Me.LinkWWW.Location = New System.Drawing.Point(47, 100)
+        Me.LinkWWW.Name = "LinkWWW"
+        Me.LinkWWW.Size = New System.Drawing.Size(111, 13)
+        Me.LinkWWW.TabIndex = 7
+        Me.LinkWWW.TabStop = True
+        Me.LinkWWW.Text = "www.excelsolutions.pl"
+        '
+        'LinkMail
+        '
+        Me.LinkMail.AutoSize = True
+        Me.LinkMail.Location = New System.Drawing.Point(47, 27)
+        Me.LinkMail.Name = "LinkMail"
+        Me.LinkMail.Size = New System.Drawing.Size(148, 13)
+        Me.LinkMail.TabIndex = 3
+        Me.LinkMail.TabStop = True
+        Me.LinkMail.Text = "lukasz.r.morawski@gmail.com"
+        '
         'Form_O_Mnie
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(434, 299)
+        Me.ClientSize = New System.Drawing.Size(227, 316)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Btn_Zamknij)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form_O_Mnie"
         Me.Text = "About program"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
@@ -173,14 +159,13 @@ Partial Class Form_O_Mnie
     End Sub
 
     Friend WithEvents Btn_Zamknij As Button
-    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents LinkMail As LinkLabel
-    Friend WithEvents LinkWWW As LinkLabel
-    Friend WithEvents Label3 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents L_Wersja As Label
+    Friend WithEvents LinkMail As LinkLabel
+    Friend WithEvents LinkWWW As LinkLabel
+    Friend WithEvents Label3 As Label
 End Class
